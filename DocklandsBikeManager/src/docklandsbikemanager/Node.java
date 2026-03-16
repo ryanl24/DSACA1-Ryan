4b1dbde17e749e1bdf4e34618d604ed1a6d6dc80
ryanl24 <148884215+ryanl24@users.noreply.github.com>
2026-03-16 22:38:56 +0000

    Node / SLL
    added the node class and SLL

--- DocklandsBikeManager/src/docklandsbikemanager/Node.java
+++ DocklandsBikeManager/src/docklandsbikemanager/Node.java
@@ -10,4 +10,41 @@ package docklandsbikemanager;
  */
 public class Node {
     
+    public Object element;
+    public Node next;
+
+    public Node(Object element){
+        this.element = element;
+        this.next = null;
+    }
+
+    public Node(Object element, Node next) {
+        this.element = element;
+        this.next = next;
+    }
+
+    public Object getElement() {
+        return element;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setElement(Object element) {
+        this.element = element;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- DocklandsBikeManager/src/docklandsbikemanager/SinglyLinkedList.java
+++ DocklandsBikeManager/src/docklandsbikemanager/SinglyLinkedList.java
@@ -10,4 +10,84 @@ package docklandsbikemanager;
  */
 public class SinglyLinkedList {
     
+    private Node head;
+    private Node curr;
+    private Node prev;
+    private int size;
+
+    public SinglyLinkedList() {
+        head = null;
+        curr = null;
+        prev = null;
+        size = 0;
+    }
+    
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    
+    public int size(){
+        return size;
+    }
+    
+    public void add(int index, Object element){
+        if(index == 1){
+            Node newNode = new Node(element, head);
+            head = newNode;
+            
+        }else if(head == null){
+            Node newNode = new Node (element);
+            head = newNode;
+            
+        }else{
+            setCurrent(index);
+            Node newNode = new Node(element, curr.getNext());
+            curr.setNext(newNode);
+            
+        }
+        
+        size++;
+    }
+    
+    private void setCurrent(int index){
+        curr = head;
+        prev = null;
+        
+        for(int i = 1; i < index; i++){
+            prev = curr;
+            curr = curr.getNext();
+        }
+    }
+    
+    public void remove(int index){
+        
+        if(index == 1){
+            head = head.getNext();
+            
+        }else{
+            setCurrent(index);
+            curr = curr.getNext();
+            prev.setNext(curr);
+        }
+        
+        size --;
+        
+    }
+    
+    public void printList(){
+        Node aNode = head;
+        
+        while(aNode != null){
+            System.out.println(aNode.getElement().toString());
+            aNode = aNode.getNext();
+        }
+    }
+    
+    
+    
+
+    
+    
+    
+    
 }
