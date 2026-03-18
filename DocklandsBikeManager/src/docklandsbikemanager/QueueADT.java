5f5bd1f1475780acef5a9898aa84cd5ecf69e06f
ryanl24 <148884215+ryanl24@users.noreply.github.com>
2026-03-18 01:46:51 +0000

    Queue
    Queue finished using arrayList

--- DocklandsBikeManager/src/docklandsbikemanager/QueueADT.java
+++ DocklandsBikeManager/src/docklandsbikemanager/QueueADT.java
@@ -4,10 +4,69 @@
  */
 package docklandsbikemanager;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ryanl
  */
-public class QueueADT {
+public class QueueADT implements QueueInterface{
+    
+    private final ArrayList<Object> theQueue;
+    
+    public QueueADT(){
+        theQueue = new ArrayList<>();
+    }
+    
+    @Override
+    public int size(){
+        return theQueue.size();
+    }
+    
+    @Override
+    public Object frontElement(){
+        if(theQueue.size() > 0){
+            return theQueue.get(0);
+        }else{
+            return null;
+        }
+    }
+    
+    @Override
+    public void enqueue(Object element){
+        theQueue.add(element);
+    }
+    
+    @Override
+    public Object dequeue(){
+        if(theQueue.size() > 0){
+            return theQueue.remove(0);
+        }else{
+            return  null;
+        }
+    }
+    
+    @Override
+    public boolean isEmpty(){
+        return theQueue.isEmpty();
+    }
+    
+    @Override
+    public String displayQueue(){
+        StringBuilder strB = new StringBuilder();
+        
+        if(theQueue.isEmpty()){
+            strB.append("Empty Queue");
+        }else{
+            for(int i = 0; i< theQueue.size(); i++){
+                strB.append(theQueue.get(i).toString());
+                strB.append("\n");
+            }
+        
+        }
+        
+        return strB.toString();
+        
+    }
     
 }
---
+++ DocklandsBikeManager/src/docklandsbikemanager/QueueInterface.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package docklandsbikemanager;
+
+/**
+ *
+ * @author ryanl
+ */
+public interface QueueInterface {
+    
+    
+    public int size();
+    public Object frontElement();
+    public void enqueue(Object element);
+    public Object dequeue();
+    public boolean isEmpty();
+    public String displayQueue();
+    
+    
+}
